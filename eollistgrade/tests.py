5459dd8641e0394204430eb7dd8090d15ce55176
Luchin101 <luis.santana101@gmail.com>
2021-12-28 16:08:39 -0300

    add import/export data in csv

--- eollistgrade/tests.py
+++ eollistgrade/tests.py
@@ -100,6 +100,7 @@ class EolListGradeXBlockTestCase(UrlResetMixin, ModuleStoreTestCase):
         """
         self.assertEqual(self.xblock.display_name, 'Notas Manuales')
         self.assertEqual(self.xblock.puntajemax, 100)
+        self.assertTrue(self.xblock.is_manual)
 
     def test_edit_block_studio(self):
         """
@@ -108,11 +109,12 @@ class EolListGradeXBlockTestCase(UrlResetMixin, ModuleStoreTestCase):
         request = TestRequest()
         request.method = 'POST'
         self.xblock.xmodule_runtime.user_is_staff = True
-        data = json.dumps({'display_name': 'testname', "puntajemax": '200'})
+        data = json.dumps({'display_name': 'testname', "puntajemax": '200', 'is_manual': False})
         request.body = data.encode()
         response = self.xblock.studio_submit(request)
         self.assertEqual(self.xblock.display_name, 'testname')
         self.assertEqual(self.xblock.puntajemax, 200)
+        self.assertFalse(self.xblock.is_manual)
 
     def test_student_view_staff(self):
         """
@@ -137,6 +139,17 @@ class EolListGradeXBlockTestCase(UrlResetMixin, ModuleStoreTestCase):
         self.assertEqual(response['calificado_equipo'], 0)
         self.assertEqual(response['lista_alumnos'][0], context_student)
         self.assertEqual(response['lista_equipo'][0], context_staff)
+
+    def test_student_view_staff_csv(self):
+        """
+            Verify context in student_view staff user when score mode is CSV
+        """
+        self.xblock.is_manual = False
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        response = self.xblock.get_context()
+        self.assertEqual(response['is_course_staff'], True)
+        self.assertFalse('lista_alumnos' in response)
 
     def test_student_view_student(self):
         """
@@ -530,3 +543,166 @@ class EolListGradeXBlockTestCase(UrlResetMixin, ModuleStoreTestCase):
         state = StudentModule.objects.get(pk=module.id)
         self.assertEqual(json.loads(state.state), json.loads('{}'))
         self.assertEqual(self.xblock.get_score(self.student.id), None)
+
+    @patch('lms.djangoapps.grades.signals.handlers.PROBLEM_WEIGHTED_SCORE_CHANGED.send')
+    @patch('eollistgrade.eollistgrade.EolListGradeXBlock.file_to_csvreader')
+    def test_import_csv(self, csv_reader, _):
+        """
+            test import_csv noraml process
+        """
+        from lms.djangoapps.courseware.models import StudentModule
+        module = StudentModule(
+            module_state_key=self.xblock.location,
+            student_id=self.student.id,
+            course_id=self.course.id,
+            state='{}')
+        module.save()
+        mock_file_object = mock.Mock()
+        mock_file_object.configure_mock(name="file_name")
+        fields = {
+            "file": mock.Mock(file=mock_file_object),
+        }
+        csv_reader.return_value = [
+            [self.student.username, self.student.email, '1', 'this is a comment']
+            ]
+        self.xblock.is_manual = False
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        response = self.xblock.import_csv(mock.Mock(method="POST", params=fields))
+        data = response.json
+        self.assertEqual(data['result'], 'success')
+        self.assertEqual(data['errors'], '')
+        self.assertEqual(data['wrong_data'], [])
+        self.assertEqual(data['total_scored'], 1)
+        state = StudentModule.objects.get(pk=module.id)
+        self.assertEqual(
+            json.loads(state.state),
+            json.loads('{"comment": "this is a comment", "student_score": 1}'))
+        self.assertEqual(self.xblock.get_score(self.student.id), 1)
+
+    def test_import_csv_wrong_method(self):
+        """
+            test import csv with wrong request method
+        """
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        fields = {
+            "file": '',
+        }
+        response = self.xblock.import_csv(mock.Mock(method="GET", params=fields))
+        self.assertEqual(response.status_code, 400)
+
+    def test_import_csv_no_file(self):
+        """
+            test import csv with no file in params
+        """
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        response = self.xblock.import_csv(mock.Mock(method="POST", params={}))
+        data = response.json
+        self.assertEqual(data['result'], 'error')
+        self.assertEqual(data['code_error'], 2)
+        self.assertEqual(data['errors'], 'File not uploaded')
+
+        response = self.xblock.import_csv(mock.Mock(method="POST", params={'file':123}))
+        data = response.json
+        self.assertEqual(data['result'], 'error')
+        self.assertEqual(data['code_error'], 2)
+        self.assertEqual(data['errors'], 'File not uploaded')
+
+    def test_import_csv_denied_user(self):
+        """
+            test import csv when user dont have permissions
+        """
+        self.xblock.xmodule_runtime.user_is_staff = False
+        self.xblock.scope_ids.user_id = self.student.id
+        response = self.xblock.import_csv(mock.Mock(method="POST", params={}))
+        data = response.json
+        self.assertEqual(data['result'], 'error')
+        self.assertEqual(data['code_error'], 1)
+        self.assertEqual(data['errors'], 'User dont have permission')
+
+    @patch('eollistgrade.eollistgrade.EolListGradeXBlock.file_to_csvreader')
+    def test_import_csv_wrong_data(self, csv_reader):
+        """
+            test import csv when csv have wrong data
+        """
+        mock_file_object = mock.Mock()
+        mock_file_object.configure_mock(name="file_name")
+        fields = {
+            "file": mock.Mock(file=mock_file_object),
+        }
+        csv_reader.return_value = [
+            ['asdasdsad', self.student.email, '1', 'this is a comment']
+            ]
+        self.xblock.is_manual = False
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        response = self.xblock.import_csv(mock.Mock(method="POST", params=fields))
+        data = response.json
+        self.assertEqual(data['result'], 'success')
+        self.assertEqual(data['errors'], '')
+        self.assertEqual(data['wrong_data'], [['asdasdsad', self.student.email, '1', 'this is a comment']])
+        self.assertEqual(data['total_scored'], 0)
+
+    def test_export_csv_wrong_method(self):
+        """
+            test export csv with wrong request method
+        """
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        response = self.xblock.export_csv(mock.Mock(method="POST"))
+        self.assertEqual(response.status_code, 400)
+
+    def test_export_csv_denied_user(self):
+        """
+            test export csv when user dont have permissions
+        """
+        self.xblock.xmodule_runtime.user_is_staff = False
+        self.xblock.scope_ids.user_id = self.student.id
+        response = self.xblock.export_csv(mock.Mock(method="GET", params={}))
+        self.assertEqual(response.status_code, 401)
+
+    def test_export_csv(self):
+        """
+            test export csv normal process
+        """
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        response = self.xblock.export_csv(mock.Mock(method="GET"))
+        data = [x.decode() for x in response.app_iter]
+        data_student = '{};{};{};{}\r\n'.format(self.student.username, self.student.email,'','')
+        data_staff = '{};{};{};{}\r\n'.format(self.staff_user.username, self.staff_user.email,'','')
+        expect = ['',"Estudiante;Email;Puntaje;Comentario\r\n", data_staff, data_student]
+        self.assertEqual(data, expect)
+
+    @patch('lms.djangoapps.grades.signals.handlers.PROBLEM_WEIGHTED_SCORE_CHANGED.send')
+    def test_export_csv_with_data(self, _):
+        """
+            test export csv normal process, with data
+        """
+        from lms.djangoapps.courseware.models import StudentModule
+        request = TestRequest()
+        request.method = 'POST'
+
+        self.xblock.xmodule_runtime.user_is_staff = True
+        self.xblock.scope_ids.user_id = self.staff_user.id
+        data = json.dumps({"id": self.student.id,
+                           "puntaje": "11",
+                           "comentario": "comentario121",
+                           "role": 'estudiante'})
+        request.body = data.encode()
+        module = StudentModule(
+            module_state_key=self.xblock.location,
+            student_id=self.student.id,
+            course_id=self.course.id,
+            state='{}')
+        module.save()
+
+        aux_response = self.xblock.savestudentanswers(request)
+        response = self.xblock.export_csv(mock.Mock(method="GET"))
+        data = [x.decode() for x in response.app_iter]
+        data_student = '{};{};{};{}\r\n'.format(self.student.username, self.student.email,'11','comentario121')
+        data_staff = '{};{};{};{}\r\n'.format(self.staff_user.username, self.staff_user.email,'','')
+        expect = ['',"Estudiante;Email;Puntaje;Comentario\r\n", data_staff, data_student]
+        self.assertEqual(data, expect)
